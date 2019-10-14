a78d9822172b5ecd7a39b930bbcaec2fe0158034
79504 <79504@DESKTOP-2V1IQJN>
2019-10-14 23:12:49 +0300

    Добавил в модальное окно свойства типа Состав , срок годности и т.д. поменял там немного стили

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -50,53 +50,76 @@ namespace Domain.Entities
         public byte[] ImageData { get; set; }
         public string ImageMimeType { get; set; }
 
-        [Display(Name = "Основная категория 1")]
-        public string MainCategory1 { get; set; }
+        //[Display(Name = "Основная категория 1")]
+        //public string MainCategory1 { get; set; }
 
-        [Display(Name = "Основная категория 2")]
-        public string MainCategory2 { get; set; }
+        //[Display(Name = "Основная категория 2")]
+        //public string MainCategory2 { get; set; }
 
-        [Display(Name = "Основная категория 3")]
-        public string MainCategory3 { get; set; }
+        //[Display(Name = "Основная категория 3")]
+        //public string MainCategory3 { get; set; }
 
-        [Display(Name = "Под Категория 1")]
-        public string SubCategory1 { get; set; }
+        //[Display(Name = "Под Категория 1")]
+        //public string SubCategory1 { get; set; }
 
-        [Display(Name = "Под Категория 2")]
-        public string SubCategory2 { get; set; }
+        //[Display(Name = "Под Категория 2")]
+        //public string SubCategory2 { get; set; }
 
-        [Display(Name = "Под Категория 3")]
-        public string SubCategory3 { get; set; }
+        //[Display(Name = "Под Категория 3")]
+        //public string SubCategory3 { get; set; }
 
-        [Display(Name = "Под Категория 4")]
-        public string SubCategory4 { get; set; }
+        //[Display(Name = "Под Категория 4")]
+        //public string SubCategory4 { get; set; }
 
-        [Display(Name = "Под Категория 5")]
-        public string SubCategory5 { get; set; }
+        //[Display(Name = "Под Категория 5")]
+        //public string SubCategory5 { get; set; }
 
-        [Display(Name = "Под Категория 6")]
-        public string SubCategory6 { get; set; }
+        //[Display(Name = "Под Категория 6")]
+        //public string SubCategory6 { get; set; }
 
+        /// <summary>
+        /// Состав
+        /// </summary>
         [Display(Name = "Состав")]
         public string Composition { get; set; }
 
+        /// <summary>
+        /// Показания к применению
+        /// </summary>
+        [Display(Name = "Показания к применению")]
+        public string IndicationsForUse { get; set; }
+
+        /// <summary>
+        /// Способ применения
+        /// </summary>
         [Display(Name = "Способ применения")]
         public string MethodOfUse { get; set; }
 
+        /// <summary>
+        /// Противопоказания
+        /// </summary>
         [Display(Name = "Противопоказания")]
         public string Contraindications { get; set; }
 
+        /// <summary>
+        /// Условия хранения
+        /// </summary>
         [Display(Name = "Условия хранения")]
         public string StorageConditions { get; set; }
 
+        /// <summary>
+        /// Срок годности
+        /// </summary>
         [Display(Name = "Срок годности")]
         public string ShelfLife { get; set; }
 
+        /// <summary>
+        /// Форма выпуска
+        /// </summary>
         [Display(Name = "Форма выпуска")]
         public string FormRelease { get; set; }
 
-        [Display(Name = "Показания к применению")]
-        public string IndicationsForUse { get; set; }
+
 
         //[Display(Name = "Картинка")]
         //public byte[] Picture { get; set; }
